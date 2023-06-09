af7b455135ed6d1de51a9cbca09309f085cdb388
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-06-09 23:57:22 +0530

    config: nextui theme setup (#3)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,18 @@
+import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import theme from "theme/theme";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider theme={theme}>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 );
