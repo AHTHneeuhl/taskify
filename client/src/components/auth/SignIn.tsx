c9386b0276997aa83d201c140c9b751e87a9a4f7
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-27 20:24:10 +0530

    chore: use useFormik hook in signUp & signIn forms

--- client/src/components/auth/SignIn.tsx
+++ client/src/components/auth/SignIn.tsx
@@ -1,84 +1,86 @@
 import { Button, Checkbox, Input, Modal, Row, Text } from "@nextui-org/react";
 import { signInSchema } from "config";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import { useSignIn, useSignInModal } from "hooks/auth";
 import Mail from "./Mail";
 import Password from "./Password";
-
-const initialFormValues = {
-  email: "",
-  password: "",
-};
 
 const SignIn: React.FC = () => {
   const { isOpen, onClose } = useSignInModal();
   const { handleSignIn } = useSignIn();
 
+  const { values, handleSubmit, handleChange, handleBlur } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: signInSchema,
+    onSubmit: (values, actions) => {
+      handleSignIn(values);
+      actions.resetForm();
+    },
+  });
+
   return (
-    <Formik
-      initialValues={initialFormValues}
-      onSubmit={(values) => {
-        console.log(values);
-        handleSignIn(values);
-      }}
-      validationSchema={signInSchema}
+    <Modal
+      closeButton
+      blur
+      aria-labelledby="sign-in"
+      open={isOpen}
+      onClose={onClose}
     >
-      {({ handleSubmit }) => (
-        <Modal
-          closeButton
-          blur
-          aria-labelledby="sign-in"
-          open={isOpen}
-          onClose={onClose}
-        >
-          <Modal.Header>
-            <Text id="sign-in" size={18}>
-              Welcome to
-              <Text b size={18}>
-                Taskify
-              </Text>
-            </Text>
-          </Modal.Header>
-          <Modal.Body>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              clearable
-              bordered
-              fullWidth
-              color="primary"
-              size="lg"
-              placeholder="Email"
-              aria-label="Email"
-              contentLeft={<Mail />}
-            />
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              clearable
-              bordered
-              fullWidth
-              color="primary"
-              size="lg"
-              placeholder="Password"
-              aria-label="Password"
-              contentLeft={<Password />}
-            />
-            <Row justify="space-between">
-              <Checkbox>
-                <Text size={14}>Remember me</Text>
-              </Checkbox>
-              <Text size={14}>Forgot password?</Text>
-            </Row>
-            <Button auto type="submit" onPress={onClose}>
-              Sign In
-            </Button>
-          </Modal.Body>
-        </Modal>
-      )}
-    </Formik>
+      <Modal.Header>
+        <Text id="sign-in" size={18}>
+          Welcome to
+          <Text b size={18}>
+            Taskify
+          </Text>
+        </Text>
+      </Modal.Header>
+      <Modal.Body as="form" onSubmit={() => handleSubmit()}>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          clearable
+          bordered
+          fullWidth
+          color="primary"
+          size="lg"
+          placeholder="Email"
+          aria-label="Email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          contentLeft={<Mail />}
+        />
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          clearable
+          bordered
+          fullWidth
+          color="primary"
+          size="lg"
+          placeholder="Password"
+          aria-label="Password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          contentLeft={<Password />}
+        />
+        <Row justify="space-between">
+          <Checkbox>
+            <Text size={14}>Remember me</Text>
+          </Checkbox>
+          <Text size={14}>Forgot password?</Text>
+        </Row>
+        <Button auto type="submit" onPress={onClose}>
+          Sign In
+        </Button>
+      </Modal.Body>
+    </Modal>
   );
 };
 
--- client/src/components/auth/SignUp.tsx
+++ client/src/components/auth/SignUp.tsx
@@ -1,100 +1,107 @@
 import { Button, Checkbox, Input, Modal, Row, Text } from "@nextui-org/react";
 import { signUpSchema } from "config";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import { useSignUp, useSignUpModal } from "hooks/auth";
 import Mail from "./Mail";
 import Password from "./Password";
-
-const initialFormValues = {
-  fullName: "",
-  email: "",
-  password: "",
-};
 
 const SignUp: React.FC = () => {
   const { isOpen, onClose } = useSignUpModal();
   const { handleSignUp } = useSignUp();
 
+  const { values, handleSubmit, handleChange, handleBlur } = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: signUpSchema,
+    onSubmit: (values, actions) => {
+      console.log(values);
+      handleSignUp(values);
+      actions.resetForm();
+    },
+  });
+
   return (
-    <Formik
-      initialValues={initialFormValues}
-      onSubmit={(values) => handleSignUp(values)}
-      validationSchema={signUpSchema}
+    <Modal
+      closeButton
+      blur
+      aria-labelledby="modal-title"
+      open={isOpen}
+      onClose={onClose}
     >
-      {({ handleSubmit }) => (
-        <Modal
-          closeButton
-          blur
-          aria-labelledby="modal-title"
-          open={isOpen}
-          onClose={onClose}
-        >
-          <form onSubmit={handleSubmit}>
-            <Modal.Header>
-              <Text id="modal-title" size={18}>
-                Welcome to
-                <Text b size={18}>
-                  Taskify
-                </Text>
-              </Text>
-            </Modal.Header>
-            <Modal.Body>
-              <Input
-                id="fullName"
-                name="fullName"
-                clearable
-                bordered
-                fullWidth
-                color="primary"
-                size="lg"
-                aria-label="Full Name"
-                placeholder="Full Name"
-              />
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                clearable
-                bordered
-                fullWidth
-                color="primary"
-                size="lg"
-                aria-label="Email"
-                placeholder="Email"
-                contentLeft={<Mail />}
-              />
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                clearable
-                bordered
-                fullWidth
-                color="primary"
-                size="lg"
-                aria-label="Password"
-                placeholder="Password"
-                contentLeft={<Password />}
-              />
-              <Row justify="space-between">
-                <Checkbox>
-                  <Text size={14}>Remember me</Text>
-                </Checkbox>
-                <Text size={14}>Forgot password?</Text>
-              </Row>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button auto flat color="error" onPress={onClose}>
-                Close
-              </Button>
-              <Button auto type="submit" onClick={onClose}>
-                Sign Up
-              </Button>
-            </Modal.Footer>
-          </form>
-        </Modal>
-      )}
-    </Formik>
+      <Modal.Header>
+        <Text id="modal-title" size={18}>
+          Welcome to
+          <Text b size={18}>
+            Taskify
+          </Text>
+        </Text>
+      </Modal.Header>
+      <Modal.Body as="form" onSubmit={() => handleSubmit()}>
+        <Input
+          id="fullName"
+          name="fullName"
+          clearable
+          bordered
+          fullWidth
+          color="primary"
+          size="lg"
+          aria-label="Full Name"
+          placeholder="Full Name"
+          value={values.fullName}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          clearable
+          bordered
+          fullWidth
+          color="primary"
+          size="lg"
+          aria-label="Email"
+          placeholder="Email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          contentLeft={<Mail />}
+        />
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          clearable
+          bordered
+          fullWidth
+          color="primary"
+          size="lg"
+          aria-label="Password"
+          placeholder="Password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          contentLeft={<Password />}
+        />
+        <Row justify="space-between">
+          <Checkbox>
+            <Text size={14}>Remember me</Text>
+          </Checkbox>
+          <Text size={14}>Forgot password?</Text>
+        </Row>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button auto flat color="error" onPress={onClose}>
+          Close
+        </Button>
+        <Button auto type="submit" onPress={onClose}>
+          Sign Up
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
