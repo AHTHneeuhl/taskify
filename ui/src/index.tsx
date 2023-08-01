33555bdb19f3134c545c08a24f5ce519f04b4ed1
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-01 13:37:16 +0530

    chore: redux store setup

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ConfigProvider, theme } from "antd";
+import { Provider } from "react-redux";
+import reduxStore from "redux/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm,
-      }}
-    >
-      <App />
-    </ConfigProvider>
-  </React.StrictMode>
+    <Provider store={reduxStore}>
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </Provider>
+  </React.StrictMode>,
 );
