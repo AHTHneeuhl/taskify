03120bb5244aad13d9d29289242da8e9ca334dfd
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-05 01:52:32 +0530

    chore: tailwindcss setup

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,6 +4,7 @@ import { ConfigProvider, theme } from "antd";
 import { Provider } from "react-redux";
 import reduxStore from "redux/store";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
