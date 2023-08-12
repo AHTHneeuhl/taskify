926c575973b79bacb8f9f4cd83eba50479291972
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-08-12 16:50:59 +0530

    chore: render dashboard and home pages (#25)

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,9 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import reduxStore from "redux/store";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -12,7 +16,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={reduxStore}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 );
