eb6f70cc05e3cfaab3245980c5f78083b4497522
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-25 14:35:07 +0530

    feat: create typography component and it's story

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,10 +2,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import reduxStore from "redux/store";
 import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import "./tailwind.css";
 
 const queryClient = new QueryClient();
 
