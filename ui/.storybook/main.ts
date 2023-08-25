25221d7fcf291f1744d98012317dc91a479147f0
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-08-25 14:36:05 +0530

    feat: create typography component and it's story (#27)

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -1,21 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-styling",
-      options: {
-        postCss: {
-          implementation: require.resolve("postcss"),
-        },
-      },
-    },
+    "@storybook/addon-styling",
   ],
   framework: {
     name: "@storybook/react-webpack5",
